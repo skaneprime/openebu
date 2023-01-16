40522c8e1084d56f71a042d55a6608c81231b2fa
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2023-01-16 18:24:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
+app.use(express.text())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/login/', (req, res) => {
@@ -22,13 +23,12 @@ app.get('/sample/', (req, res) => {
 
 app.all('/result4/', (req, res, next) => {
     res.set({
-        'X-Author': 'itmo336261',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
         'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
         'Content-Type': 'application/json'    
     })
-    const data = { 'message': 'itmo336261', 'x-result': req.headers['x-test'] || null, 'x-body': req.body || null };
+    const data = { 'message': 'itmo336261', 'x-result': req.headers['x-test'], 'x-body': req.body };
     console.log('this is', data);
     res.send(JSON.stringify(data));
     next();
