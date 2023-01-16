3f53440166fd06d15220ce11f298204419e82bb7
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2023-01-16 17:52:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.get('/sample/', (req, res) => {
     res.send('function task(x) { return x * x }');
 });
 
+app.get('/result4/', (req, res) => {
+    res.set({
+        'X-Author': 'itmo336261',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'text/plain; charset=UTF-8'    
+    })
+    res.send(JSON.stringify({ 'message': 'itmo336261', 'x-result': req.headers['x-test'], 'x-body': req.body }));
+});
+
 app.listen(process.env.PORT || 3030, () => {
   console.log(`server started on port ${process.env.PORT || 3030}`);
 });
